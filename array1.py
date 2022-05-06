92df9b841537a16cec32d5824aad127cb85c6089
AditiPanda <panda38aditi@gmail.com>
2022-05-06 14:15:36 +0530

    Add files via upload

--- array1.py
+++ array1.py
@@ -11,19 +11,39 @@ print(arr1)
 # append works in both
 arr.append(4)
 arr1.append(4)
-
+print('Appending..')
 print(arr[3])
 print(arr1)
 
 # so does insert!
 arr.insert(3, 60)
 arr1.insert(3, 60)
-
+print('Inserts!')
 print(arr[3])
 print(arr1)
 
 # to print the contents of array created using array module:
 for i in range(len(arr)):
     print(arr[i], end='\t')
-
 print('\n')
+
+# pop(pos) - to remove elt at pos
+print('Waz popping?')
+arr.pop(3)
+for i in range(len(arr)):
+    print(arr[i], end='\t')
+print('\n')
+
+arr1.pop(3)
+print(arr1)
+
+# remove(elt) removes 1st occurence of elt
+arr.append(1)
+arr1.append(1)
+print('Removals..!')
+arr.remove(1)
+for i in range(len(arr)):
+    print(arr[i], end='\t')
+print('\n')
+arr1.remove(1)
+print(arr1)
