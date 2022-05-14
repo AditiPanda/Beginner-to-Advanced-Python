93eeed0a8c4b988ee968e14a862d975635c677b7
AditiPanda <panda38aditi@gmail.com>
2022-05-14 18:52:12 +0530

    Add files via upload

---
+++ map_func.py
@@ -0,0 +1,41 @@
+org_list = [1,2,3,4,5]
+
+fin_list = []
+
+for num in org_list:
+    fin_list.append(num**3)
+
+print(fin_list)
+
+
+##################
+print('Using Maps!!')
+
+def cube(num):
+    return num**3
+
+fin_list_new = []
+
+fin_list_new = list(map(cube, org_list))
+print(fin_list_new)
+
+######################
+print('Using Maps and Lambda too!!')
+
+fin_list_new_1 = []
+
+fin_list_new_1 = list(map(lambda x:x**3, org_list))
+print(fin_list_new_1)
+####################
+
+base = [1,2,3,4]
+power = [1,2,3,4]
+
+ans_list = []
+
+ans_list = list(map(lambda x,a:x**a, base, power)) # can use pow function instead
+print(ans_list)
+
+org_list = ['lana', 'del', 'rey', 'random']
+ans_list = list(map(len, org_list))
+print(ans_list)
